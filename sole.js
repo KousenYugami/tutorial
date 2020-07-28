2b86f53b752a573c03ed3f07a8e6294ee75de610
JohnHannibal <46013926+JohnHannibal@users.noreply.github.com>
2020-07-28 13:14:17 +0800

    Day 3

--- sole.js
+++ sole.js
@@ -1,5 +1,12 @@
 var name = "光線ゆがみ";
 var age = 20;
-var message = "こんいちは！　"　+ name + "　と申します。" + age + "ー際です";
+var linesCoded = 15;
+var message = "こんいちは！　"　+ name + "　と申します。" + age + "際です";
+if (age >= 18){
+    console.log('You are now of legal age!');
+}
+for (lineWritten = 1; lineWritten <= linesCoded; lineWritten++){
+    console.log('You have written ' + lineWritten + ' lines of code!');
+}
 alert(message);
 //console.log(message);
